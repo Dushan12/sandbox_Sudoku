d9c9000739605f5ab88d5b82e65a539873109b5e
Dushan12 <gajikdushan@gmail.com>
2024-09-10 15:38:05 +0200

    Add tests for grid validation

--- Sudoku/src/main/scala/com/sudoku/services/ValidationService.scala
+++ Sudoku/src/main/scala/com/sudoku/services/ValidationService.scala
@@ -5,12 +5,23 @@ import zio.ZIO
 
 class ValidationService {
 
-  def validate(sudokuBoard: SudokuBoard): ZIO[Any, Nothing, Boolean] = {
+  def isValid(sudokuBoard: SudokuBoard): ZIO[Any, Nothing, Boolean] = {
 
-
-    ZIO.succeed(sudokuBoard.items.flatMap(_.map(_.value)).exists { value =>
-      value <= 0 && value > 9
+    val hasInvalidNumbers = ZIO.succeed(sudokuBoard.items.flatMap(_.map(_.value)).exists { value =>
+      value < 1 || value > 9
     })
+
+    val invalidNumberOfColumns = ZIO.succeed(sudokuBoard.items.length != 9)
+
+    val invalidNumberOfRows = ZIO.succeed(sudokuBoard.items.map(_.length).map { value => value == 9  }.exists(x => !x))
+
+    for {
+      invalidNumbers <- hasInvalidNumbers
+      invalidColumns <- invalidNumberOfColumns
+      invalidRows <- invalidNumberOfRows
+    } yield (!(invalidNumbers || invalidColumns || invalidRows))
+
+
   }
 
 }
--- Sudoku/src/test/scala/com/sudoku/services/validationservice/ValidateTest.scala
+++ Sudoku/src/test/scala/com/sudoku/services/validationservice/ValidateTest.scala
@@ -8,6 +8,55 @@ object ValidateTest extends ZIOSpecDefault {
 
   def spec: Spec[Any, Nothing] =
     suite("Sudoku -> Validate Service -> Specs")(
+
+     test("Return false when grid has less than 9 columns") {
+
+        val target = new ValidationService()
+
+        val input = SudokuBoard(List.fill(8)(List.fill(9)(SudokuCell(1))))
+
+        for {
+          actual <- target.isValid(input)
+        } yield assertTrue(!actual)
+
+      },
+
+      test("Return false when grid has more than 9 columns") {
+
+        val target = new ValidationService()
+
+        val input = SudokuBoard(List.fill(10)(List.fill(9)(SudokuCell(1))))
+
+        for {
+          actual <- target.isValid(input)
+        } yield assertTrue(!actual)
+
+      },
+
+      test("Return false when at least one column has more or less than 9 rows") {
+
+        val target = new ValidationService()
+
+        val input = SudokuBoard(List.fill(9)(List.fill(8)(SudokuCell(1))))
+
+        for {
+          actual <- target.isValid(input)
+        } yield assertTrue(!actual)
+
+      },
+
+      test("Return false when at least one column has more or less than 9 rows") {
+
+        val target = new ValidationService()
+
+        val input = SudokuBoard(List.fill(9)(List.fill(10)(SudokuCell(1))))
+
+        for {
+          actual <- target.isValid(input)
+        } yield assertTrue(!actual)
+
+      },
+
       test("Return false when data is not valid (at least one number is not between 1 and 9)") {
 
         val target = new ValidationService()
@@ -15,7 +64,7 @@ object ValidateTest extends ZIOSpecDefault {
         val input = SudokuBoard(List.fill(9)(List.fill(9)(SudokuCell(0))))
 
         for {
-          actual <- target.validate(input)
+          actual <- target.isValid(input)
         } yield assertTrue(!actual)
 
       }
