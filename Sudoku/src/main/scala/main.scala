fe63933a2ba000cfb6a9eda75ea9762d69ea1e17
Dushan12 <gajikdushan@gmail.com>
2024-09-11 12:00:21 +0200

    Add zio http validate method

--- Sudoku/src/main/scala/main.scala
+++ Sudoku/src/main/scala/main.scala
@@ -1,4 +1,5 @@
-import com.sudoku.utils.{BoardGeneratorUtil, PrintUtil}
+import com.sudoku.services.ValidationService
+import com.sudoku.utils.{BoardGeneratorUtil, JsonUtils, PrintUtil}
 import zio.*
 import zio.http.*
 
@@ -6,12 +7,26 @@ object main extends ZIOAppDefault {
 
   private val routes: Routes[Any, Nothing] =
     Routes(
-      Method.GET / "generate" -> handler(Response.text("Greetings at your service"))
+      Method.GET / "generate" -> handler(Response.text("Greetings at your service")),
+      Method.POST / "validate" -> handler { (req: Request) =>
+        val parseJsonUtils = new JsonUtils()
+        val validationService = new ValidationService()
+        (for {
+          bodyStr <- req.body.asString.orElse(ZIO.succeed(""))
+          parseBodyAsSudokuBoard <- parseJsonUtils.fromJson(bodyStr)
+          isValid <- validationService.isValid(parseBodyAsSudokuBoard)
+        } yield {
+          if (isValid)
+            Response.text("""Board is valid!""")
+          else
+            Response.text("""Board is invalid!""")
+        }).catchAll { _ =>
+          ZIO.succeed(Response.text("""Board is invalid!"""))
+        }
+
+      }
     )
 
-
-
-  
   def run: ZIO[ZIOAppArgs & Scope, Any, Any] = Server.serve(routes).provide(Server.default)
 
 }
